604766a3fc904cb520f5038a8f6c1ca7aa8f07fb
laureanray <laureanraybahala@gmail.com>
2023-05-30 23:31:04 +0800

    add poker point component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+type PointProps = { point: string };
+
+const Points: Component<PointProps> = (props) => {
+  return (
+    <button class="border-2 border-slate-900 rounded-sm p-10 m-1 w-24 h-32 flex justify-center">
+      <span class="text-3xl font-bold text-slate-900">{props.point}</span>
+    </button>
+  );
+};
+
+const PointsWrapper: Component = () => {
+  const points = ["0", "1/2", "1", "2", "3", "5", "8", "13", "20", "40", "100", "?"];
+
+  return (
+    <div class="w-content full grid grid-cols-6 gap-0">
+      {points.map((p, idx) => (
+        <Points point={p} />
+      ))}
+    </div>
+  );
+};
+
+const PokerPlanner: Component = () => {};
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <h1 className="text-red-500 font-bold text-3xl"> Hello Solid! </h1>
+    <div class="flex">
+      <PointsWrapper />
     </div>
   );
 };
